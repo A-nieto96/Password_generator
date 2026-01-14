905de8c35b70c1ac0bf98b23c28c9e3b99fba691
A-nieto96 <adrianfnieto1996@gmail.com>
2026-01-14 01:30:46 -0500

    correccion de deploy

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import string
 import secrets
+import os   
 
 app = Flask(__name__)
 
@@ -68,4 +69,5 @@ def index():
     return render_template("index.html", contrasena=contrasena, error=error)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
